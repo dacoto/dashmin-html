2b0ed3de8c7f36191798381fdd4faec15644b044
David Cortés <dacoto97@hotmail.com>
2018-08-09 03:14:51 +0200

    Corregidos error de actualización

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,6 @@
 window.$ = window.jQuery = require('jquery');
 require('popper.js');
 require('bootstrap');
-window.alertify = require('alertifyjs');
 window.List = require('list.js');
 window.swal = require('sweetalert');
 require('select2');
